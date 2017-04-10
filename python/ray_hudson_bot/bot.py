09d26bba7b77ff5a69c0600da61bbafcd1da357b
Tim Hutchinson <t.hutchinson@utah.edu>
2017-04-10 14:38:02 -0600

    heartbeat

--- python/ray_hudson_bot/bot.py
+++ python/ray_hudson_bot/bot.py
@@ -2,7 +2,16 @@ import time
 
 import praw
 
-done = []
+players = ['messi', 'neymar']
 
 r = praw.Reddit('ray_hudson_bot', user_agent='Ray Hudson comment bot for r/soccer v0.1 by tskee2')
+subreddit = r.subreddit('soccer')
 
+for submission in subreddit.stream.submissions():
+    print submission.title
+    for player in players:
+        if player in submission.title.lower():
+            with open('../../data/%s.txt' % player, 'r') as f:
+                # count lines, pick random one, post reply
+                pass
+            break
