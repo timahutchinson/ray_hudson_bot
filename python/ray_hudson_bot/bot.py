89ac56eba5c4022e46237f13e07f4932f403d6d4
Tim Hutchinson <t.hutchinson@utah.edu>
2017-04-12 11:32:39 -0600

    up and running

--- python/ray_hudson_bot/bot.py
+++ python/ray_hudson_bot/bot.py
@@ -1,17 +1,38 @@
+from sys import stderr
+import random
 import time
 
 import praw
 
-players = ['messi', 'neymar']
+players = ['messi', 'di maria']
 
 r = praw.Reddit('ray_hudson_bot', user_agent='Ray Hudson comment bot for r/soccer v0.1 by tskee2')
-subreddit = r.subreddit('soccer')
+subreddit = r.subreddit('soccer+barca')
 
-for submission in subreddit.stream.submissions():
-    print submission.title
-    for player in players:
-        if player in submission.title.lower():
-            with open('../../data/%s.txt' % player, 'r') as f:
-                # count lines, pick random one, post reply
-                pass
-            break
+j = 0
+replied = []
+while True:
+    print "Waking..."
+    i = 0
+    for submission in subreddit.new(limit=10):
+        i += 1
+        if submission.shortlink not in replied:
+            print "Checking submission #%s..." % i
+            for player in players:
+                if player in submission.title.lower():
+                    with open('../../data/%s.txt' % player, 'r') as f:
+                        print "Replying to post: %s ..." % submission.shortlink
+                        lines = f.readlines()
+                        line = lines[random.randint(0,len(lines)-1)]
+                        submission.reply(line)
+                        replied.append(submission.shortlink)
+                        break
+    j += 1
+    if j == 11:
+        replied = []
+        j = 0
+    for i in range(60,0,-1):
+        stderr.write("\rSleeping for %s seconds..." % i)
+        time.sleep(1)
+    print ''
+    
